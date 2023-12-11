6e24f7c3943cc838fbf4d1302116498f705d2aa9
Phan Anh <phananh1904@gmail.com>
2023-12-11 14:34:52 +0700

    Add Logout.cs

---
+++ CNPM Work/Controller/Logout.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNPM_Work.Controller
+{
+    internal class Logout
+    {
+    }
+}
